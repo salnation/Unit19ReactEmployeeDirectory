312ba81740574ef213c97ab412413f188aec2c68
Stefan Salituro <stefansalituro@gmail.com>
2020-10-20 20:31:39 -0400

    database created in m ongodb.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -38,8 +38,7 @@ const isLocalhost = Boolean(
           // This is running on localhost. Let's check if a service worker still exists or not.
           checkValidServiceWorker(swUrl, config);
   
-          // Add some additional logging to localhost, pointing developers to the
-          // service worker/PWA documentation.
+          
           navigator.serviceWorker.ready.then(() => {
             console.log(
               'This web app is being served cache-first by a service ' +
@@ -66,25 +65,21 @@ const isLocalhost = Boolean(
           installingWorker.onstatechange = () => {
             if (installingWorker.state === 'installed') {
               if (navigator.serviceWorker.controller) {
-                // At this point, the updated precached content has been fetched,
-                // but the previous service worker will still serve the older
-                // content until all client tabs are closed.
+                
                 console.log(
                   'New content is available and will be used when all ' +
                     'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
                 );
   
-                // Execute callback
+                
                 if (config && config.onUpdate) {
                   config.onUpdate(registration);
                 }
               } else {
-                // At this point, everything has been precached.
-                // It's the perfect time to display a
-                // "Content is cached for offline use." message.
+                
                 console.log('Content is cached for offline use.');
   
-                // Execute callback
+          
                 if (config && config.onSuccess) {
                   config.onSuccess(registration);
                 }
@@ -110,7 +105,7 @@ const isLocalhost = Boolean(
           response.status === 404 ||
           (contentType != null && contentType.indexOf('javascript') === -1)
         ) {
-          // No service worker found. Probably a different app. Reload the page.
+          
           navigator.serviceWorker.ready.then(registration => {
             registration.unregister().then(() => {
               window.location.reload();
